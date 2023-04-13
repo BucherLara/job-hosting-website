f305ec4b2937905198a478108783615b55be8e82
Kevin Tomas <kevintomas@macbook-pro-4.speedport.ip>
2023-04-13 13:10:13 +0200

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Card from "../../components";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
