ac439b570e1fd1daee06e13fcef47032d7793a1a
kevintomas1995 <k.tomas@MacBook-Pro-von-ktomas.local>
2023-04-17 10:28:06 +0200

    add appwrite

---
+++ components/index.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+const dummyData = [
+  {
+    id: 1,
+    title: "chef",
+    company: "cooking Academy",
+    location: "Barcelona",
+    description: "super duper and very well paid job",
+    date: "01.01.2000",
+  },
+
+  {
+    id: 2,
+    title: "postman",
+    company: "postman Academy",
+    location: "Leibzig",
+    description: "letters and postcards",
+    date: "14.05.2010",
+  },
+  {
+    id: 3,
+    title: "driver",
+    company: "uber",
+    location: "London",
+    description: "you need good navigation",
+    date: "10.10.2015",
+  },
+  {
+    id: 4,
+    title: "teacher",
+    company: "school",
+    location: "Hamburg",
+    description: "you need to love kids and teach them",
+    date: "01.07.2020",
+  },
+];
+
+export default function Card({ data }) {
+  return (
+    <Container>
+      <StyledP>All job listings</StyledP>
+      {data?.documents?.length === 0 && <p>No jobs available</p>}
+      {data?.documents?.map((card) => (
+        <StyledSection>
+          <Title>{card.title}</Title>
+          <p>{card.company}</p>
+          <p>{card.location}</p>
+          <Date>{card.date}</Date>
+        </StyledSection>
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.p`
+  font-weight: bold;
+`;
+
+const StyledSection = styled.section`
+  border: 1px solid black;
+  text-align: center;
+  margin: 2em;
+  padding: 0.5em;
+  border-radius: 10px;
+  width: 40%;
+`;
+
+const StyledP = styled.p`
+  text-align: center;
+`;
+
+const Date = styled.p`
+  margin-top: 2em;
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,8 @@
 import Card from "../../components/Card";
+import { Inter } from "next/font/google";
+import { Client, Databases } from "appwrite";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ data }) {
   return (
@@ -6,4 +10,28 @@ export default function Home({ data }) {
       <Card data={data} />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const client = new Client();
+
+    client
+      .setEndpoint("http://localhost/v1")
+      .setProject("643d00957bd598fa496e");
+
+    const databases = new Databases(client);
+
+    const data = await databases.listDocuments("jobs", "643d00e3b2a4467b56ab");
+
+    return {
+      props: { data },
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      props: { data: null },
+    };
+  }
 }
